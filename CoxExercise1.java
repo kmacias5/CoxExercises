292e2dc2692ea0b1586f3977a2b3d912e1f4a689
kmacias5 <kmacias5@miners.utep.edu>
2019-02-15 16:27:07 -0600

    Update CoxExercise1.java

--- CoxExercise1.java
+++ CoxExercise1.java
@@ -1,5 +1,9 @@
 
+/**In the language of your choice, write a method that modifies a string using the following rules:
 
+1. Each word in the input string is replaced with the following: the first letter of the word, the count of distinct letters between the first and last letter, and the last letter of the word. For example, “Automotive parts" would be replaced by "A6e p3s".
+2. A "word" is defined as a sequence of alphabetic characters, delimited by any non-alphabetic characters.
+3. Any non-alphabetic character in the input string should appear in the output string in its original relative location.**/
 import java.util.Scanner;
 
 public class CoxExercise1 {
